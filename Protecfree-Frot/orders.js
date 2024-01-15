1282134d185525b4c7d62e1edd9ee85468856f41
Mauricio-Domingos22 <mauriciobete1999@gmail.com>
2024-01-15 22:45:11 +1345

    add ft

---
+++ Protecfree-Frot/orders.js
@@ -0,0 +1,21 @@
+const Orders=[
+    {
+        productName: 'javasScript Tutorial',
+        productNumber: '85743',
+        paymentStatus:'Due',
+        status:'Pending'
+    },
+    {
+        productName: 'Css Full Couse',
+        productNumber: '97245',
+        paymentStatus:'Refunded',
+        status:'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentStatus:'Paid',
+        status:'Active'
+    },
+
+]
